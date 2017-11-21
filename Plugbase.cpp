44c7f28293777021103a80e8006b59ababa50e5c
rizwanchishti <rizwanchishti@users.noreply.github.com>
2017-11-21 13:04:28 +0400

    Add files via upload

---
+++ Plugbase.cpp
@@ -0,0 +1,27 @@
+// Plugbase.cpp: implementation of the CPlugbase class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "IFServer.h"
+#include "Plugbase.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CPlugbase::CPlugbase()
+{
+
+}
+
+CPlugbase::~CPlugbase()
+{
+
+}
